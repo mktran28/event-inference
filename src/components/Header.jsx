9f694bc598682b9ef037997b40815498846e983d
Mai Khanh <mktran28@colby.edu>
2026-01-26 23:33:27 -0500

    Add acknowledgement

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ export default function Header() {
         <div className = "bg-blue text-purple p-10 text-center font-amaticsc">
             <h1 className = "text-5xl font-bold">Epidemic-Type Aftershock (ETAS)</h1>
             <h1 className = "text-xl font-bold">The Mathematical Foundation of Predictive Policing</h1>
+            <h1 className = "text-lg font-bold">Inspired by "Predictive Policing: A Mathematical Primer" by Johnson, J., McKenzie, T., & Wong, T. A.</h1>
         </div>
     )
 }
